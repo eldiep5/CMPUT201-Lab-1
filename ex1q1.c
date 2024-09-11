d29d21e24a11654be5923f48236c6578c1a73980
unknown <eldiep@ualberta.ca>
2024-09-10 19:50:05 -0600

    Added file ex1q1.c

---
+++ ex1q1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+float pi = 3.14;
+
+
+int main(void)  {
+
+float velocity;
+float radius;
+float time;
+
+printf("Enter an orbital speed (km/h): ");
+scanf("%f", velocity);
+printf("\nEnter an orbital radius (km): ");
+scanf("%f", radius);
+printf("\nEnter an orbital radius (km): ");
+scanf("%f", time);
+
+
+printf("\n%f, %f, %f", velocity, radius, time)
+return
+
+
+}
